16f7e9ff568c58af2136e0e500db6593a776329c
arcblast <adarenas@up.edu.ph>
2020-10-30 19:40:24 +0800

    terminate not working.

--- arenascabuslay.erl
+++ arenascabuslay.erl
@@ -2,66 +2,83 @@
 -compile(export_all).
 
 chat_init() ->
-	String = io:get_line ("Enter your name: "),
-	Name = string:trim(String, trailing, "\n."),
-	register(chatServer1,spawn(arenascabuslay,chatServer,[])),%So Node1 can receive messages
-	register(node1, spawn(arenascabuslay,node1,[Name])).
+	% String = io:get_line ("Enter your name: "),
+	% Name = string:trim(String, trailing, "\n."),
+	register(nodeInbox1,spawn(arenascabuslay,nodeInbox,[])),%Node1 inbox
+	register(node1,spawn(arenascabuslay,node1,["Jim", empty, empty])).%Spawn node1 with empty parameters
 
-node1(Name) ->
+node1(Name, Node2_Name, Chat_Pid)->
+	if
+		Node2_Name==empty -> %Not yet received Node2_Name or inbox Pid, so do nothing
+			io:format("You have logged in~n"),
+			Message_Init=empty;
+		true -> %Received node2 inbox, can send messages now
+			Message_Init = io:get_line ("You: "),
+			Chat_Pid ! {Name, Message_Init, self(), {node2,Node2_Name} },
+				if
+					Message_Init=="bye" ->
+						% io:format("~s ~n", [Node2_Name]),
+
+						Node2_Name ! quit, %Doesn't work
+
+						io:format("You have disconnected~n"),
+						init:stop();
+					true -> 
+						node1(Name, Node2_Name,Chat_Pid)
+				end	
+	end,
+
+
 	receive
-		{node2, Node2_Pid,Chat_Pid} ->
-			Message_Init = string:chomp(io:get_line ("(Node1)You: ")),
-			Chat_Pid ! {Name, Message_Init},
-			self() ! {node2, Node2_Pid,Chat_Pid},%Send message to self to loop this part
-			node1(Name)
-	end,
-	node1(Name).
+		{Node2_Name_Initial, Chat_Pid_Initial} ->%Receive the Node2 inbox and name
+			io:format("Received~n"),
+			node1(Name, Node2_Name_Initial,Chat_Pid_Initial);
+			
+		quit -> %if receive quit from node2, disconnect
+			init:stop(),
+			io:format("node1 Your partner has disconnected~n")
+		
+	end.
 
-
-chatServer() -> %Both nodes have one instance of this used to receive messages and print from the other node
+nodeInbox() -> %Both nodes have one instance of this used to receive messages and print from the other node
 	receive 
-		{Node_Name, Node_Message} ->
+		{Node_Name, Node_Message,Sender_Pid,Receiver_Pid} ->
 			io:format("[Chat] ~s: ",[Node_Name]),%print from node1
-			io:format("~s ~n",[Node_Message]);
-		{chat2,Name,Message} ->
-			io:format("meow. ~n")
+			io:format("~s ~n",[Node_Message]),
+			if
+				Node_Message=="bye" ->
+					Receiver_Pid ! quit,
+					io:format("~s has disconnected~n",[Node_Name]);	
+				true -> ok %Don't need to do anything if false
+			end
 	end,
+nodeInbox().
 
-chatServer().
+
 
 
 chat_init2(Node1_Node) ->
-	String2 = io:get_line ("Enter your name: "),
-	Name2 = string:trim(String2, trailing, "\n."),
-	Chat_Pid = spawn(arenascabuslay,chatServer,[]), %Allows node2 to accept messages from node1
-	spawn(arenascabuslay,node2_init,[Name2,Node1_Node,Chat_Pid]).
+	Name2="Jimmy",
+	Chat_Pid = spawn(arenascabuslay,nodeInbox,[]), %Spawn inbox of Node2
+	{node1, Node1_Node} ! {self(),Chat_Pid}, %Send node1 the inbox of Node2
+	
+	node2(Name2, Node1_Node,Chat_Pid). %If I use this instead of ^ spawn, it works? Ba't ganun
 
-
-node2_init(Name2,Node1_Node,Chat_Pid) ->
-	{node1, Node1_Node} ! {node2, self()},
-	node2(Name2,Node1_Node,Chat_Pid).
 
 node2(Name2,Node1_Node,Chat_Pid) ->
-	{node1, Node1_Node} ! {node2, self(),Chat_Pid},
-	self() ! {node2},
-	receive
-		{node2} ->%receive at the start
-			Message = string:chomp(io:get_line ("(Node2) You: ")), %Remove trailing \n
-			% Node1_Pid ! {node2, self(), Name2, Message},
-			{chatServer1,Node1_Node} ! {Name2, Message},
-			self() ! {node2},
-
-			node2(Name2,Node1_Node,Chat_Pid);
-		{node1, Node1_Pid, Node_Name, Node_Message} ->
-			io:format("Meow~n")
-			% io:format("~s: ",[Node_Name]),%printing message from node1
-			% io:format("~s ~n",[Node_Message])
-
-			% Message = io:get_line ("You: "),
-			% case Message of
-			% 	[$b,$y,$e,$\n] -> io:format("Goodbye. ~n");
-			% 	_ -> Node1_Pid ! {node2, self(), Name2, Message},
-			% 		node2(Name2,Node1_Node)
-			% end
+	Message = string:chomp(io:get_line("Input: ")),
+	{nodeInbox1,Node1_Node} ! {Name2, Message,self(),{node1, Node1_Node}}, % Send message to other node
+	if
+		Message=="bye" ->
+			io:format("You have disconnected~n"),
+			{node1,Node1_Node} ! quit, % < Doesn't work, this needs to work so node1 will terminate
+			init:stop();
+		true -> 
+			node2(Name2,Node1_Node,Chat_Pid) % Don't need to do anything if false
 	end,
-	node2(Name2,Node1_Node,Chat_Pid).
+	
+	receive
+		quit ->
+			init:stop(),
+			io:format("Your partner has disconnected~n")
+	end.
